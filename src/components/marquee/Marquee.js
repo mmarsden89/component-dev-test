7bec6b2c3a600c75e9031d1fb259e787943eccda
mmarsden89 <mattmarsdendev@gmail.com>
2021-04-04 12:26:09 -0400

    add calltoaction component into marquee level as well as general styling

--- src/components/marquee/Marquee.js
+++ src/components/marquee/Marquee.js
@@ -1,16 +1,26 @@
 import content from "../../assets/content.json";
 import "./Marquee.css";
 import image from "../../assets/backgrounds/slide_one.jpg";
+import CallToAction from "./calltoaction/CallToAction.js";
 
 const Marquet = (props) => {
   console.log(props);
+  const { currentPage } = props;
   return (
     <div
       className="marquee-container"
       style={{
         backgroundImage: `url(${image})`,
       }}
-    ></div>
+    >
+      <div className="marquee-content">
+        <h1 className="marquee-headline">
+          {props.currentPage.blocks[0].headline}
+        </h1>
+        <p className="marquee-subhead">{currentPage.blocks[0].subhead}</p>
+      </div>
+      <CallToAction ctaText={currentPage.blocks[0].cta} />
+    </div>
   );
 };
 
