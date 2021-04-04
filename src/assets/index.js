9b57e38cac3864d1d65558a985358107bee6c1a5
mmarsden89 <mattmarsdendev@gmail.com>
2021-04-04 17:16:27 -0400

    add asset index for easier importing

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import slide_one from "./backgrounds/slide_one.jpg";
+import slide_two from "./backgrounds/slide_two.jpg";
+import slide_three from "./backgrounds/slide_three.jpg";
+
+import abc_logo from "./abc_logo.svg";
+
+import arrow from "./arrow.png";
+
+import content from "./content.json";
+
+export { slide_one, slide_two, slide_three, abc_logo, arrow, content };
