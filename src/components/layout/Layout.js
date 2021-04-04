4b4d1e5954d161130c3e03e46debf7df7a918607
mmarsden89 <mattmarsdendev@gmail.com>
2021-04-04 19:28:04 -0400

    general styling

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,9 +3,14 @@ import "./Layout.css";
 
 const Layout = (props) => {
   const { currentPage, handlePageChange } = props;
+
+  const handleCurrent = (slug) => {
+    return slug === currentPage ? "nav-link active" : "nav-link";
+  };
+
   const linkMap = content.pages.map((item) => (
     <div
-      className={item.slug === currentPage ? "nav-link active" : "nav-link"}
+      className={handleCurrent(item.slug)}
       onClick={() => handlePageChange(item.slug)}
       id={item.slug}
     >
@@ -17,7 +22,7 @@ const Layout = (props) => {
     <div className="layout-container">
       <div className="menu-container">
         <div>
-          <img src={abc_logo} />
+          <img src={abc_logo} className="logo" />
           <div className="nav-link-container">{linkMap}</div>
         </div>
         <div>
