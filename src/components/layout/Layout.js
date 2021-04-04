7f187efc824d22a246bbaa0dea834f972ed0302a
mmarsden89 <mattmarsdendev@gmail.com>
2021-04-04 12:26:18 -0400

    general layout work

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,21 @@
 import abc_logo from "../../assets/abc_logo.svg";
 import "./Layout.css";
+import content from "../../assets/content.json";
+
+const linkMap = content.pages.map((item) => (
+  <div className="nav-link">{item.title}</div>
+));
 
 const Layout = () => {
   return (
     <div className="layout-container">
-      <img src={abc_logo} />
+      <div>
+        <img src={abc_logo} />
+        {linkMap}
+      </div>
+      <div>
+        <button className="contact-us">Contact Us</button>
+      </div>
     </div>
   );
 };
