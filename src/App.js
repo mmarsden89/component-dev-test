384340ef389bb4878edfdf03b904fa712f4b39ca
mmarsden89 <mattmarsdendev@gmail.com>
2021-04-05 11:38:10 -0400

    remove forced image cache function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import "./App.css";
 import Root from "./components/root/Root";
 import React, { useEffect } from "react";
-import { slide_one, slide_two, slide_three } from "./assets/index.js";
 
 function App() {
-  useEffect(() => {
-    const images = [slide_one, slide_two, slide_three];
-    images.forEach((picture) => {
-      const img = new Image();
-      img.src = picture;
-    });
-  }, []);
-
-  const cacheImages = async (arr) => {
-    const promises = await arr.map((src) => {
-      return new Promise(function (resolve, reject) {
-        const img = new Image();
-
-        img.src = src;
-        img.onload = resolve();
-        img.onerror = reject();
-      });
-    });
-
-    await Promise.all(promises);
-    console.log("done");
-  };
   return (
     <div className="App">
       <Root />
